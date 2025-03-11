84e370dbb4718dc4242afdfaafb4c22732f6bd26
Thiago Menezes <thiago.mennezes@gmail.com>
2025-03-11 08:57:37 -0400

    feat: login screen

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { ReactNode } from "react";
+import { Text, View, StyleSheet } from "react-native";
+
+export default function HomePage() {
+  return (
+    <View>
+      <Text>Hello World!</Text>
+    </View>
+  );
+}
---
+++ app/login.tsx
@@ -0,0 +1,80 @@
+import React from "react";
+import {
+  Button,
+  Image,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+
+const LoginScreen = () => {
+  return (
+    <View style={styles.container}>
+      {/* Logo Image */}
+      <Image
+        source={require("../assets/images/inpalm-banner.png")}
+        style={styles.logo}
+        resizeMode="contain"
+      />
+
+      {/* Email Input */}
+      <TextInput
+        placeholder="E-mail"
+        style={styles.input}
+        keyboardType="email-address"
+      />
+
+      {/* Password Input */}
+      <TextInput placeholder="Senha" style={styles.input} secureTextEntry />
+
+      {/* Authenticate Button */}
+      <Button
+        onPress={() => console.log("Authenticate Pressed")}
+        style={styles.button}
+        title="Autenticar"
+      />
+
+      {/* Forgot Password Link */}
+      <TouchableOpacity onPress={() => console.log("Forgot Password Pressed")}>
+        <Text style={styles.forgotPassword}>Esqueceu a senha?</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = {
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#fff",
+    padding: 20,
+  },
+  logo: {
+    width: 250,
+    height: 120,
+    marginBottom: 20,
+  },
+  input: {
+    width: "100%",
+    height: 50,
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderRadius: 8,
+    paddingLeft: 10,
+    marginBottom: 15,
+    fontSize: 16,
+  },
+  button: {
+    width: "100%",
+    paddingVertical: 10,
+  },
+  forgotPassword: {
+    marginTop: 10,
+    color: "#007bff",
+    textDecorationLine: "underline",
+  },
+};
+
+export default LoginScreen;
